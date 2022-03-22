8f15d76aac867e579e59e601af9e5d6c69f7f986
lurdharry <azeezhammed57@gmail.com>
2022-03-22 23:04:01 +0100

    feat: implement  email notiifcation  for close auctions

--- src/lib/closeAuction.js
+++ src/lib/closeAuction.js
@@ -1,7 +1,8 @@
-import AWS from 'aws-sdk';
+import AWS from "aws-sdk";
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
-const { AUCTIONS_TABLE_NAME } = process.env;
+const sqs = new AWS.SQS();
+const { AUCTIONS_TABLE_NAME, MAIL_QUEUE_URL } = process.env;
 
 export const closeAuction = async (auction) => {
   const params = {
@@ -9,16 +10,40 @@ export const closeAuction = async (auction) => {
     Key: {
       id: auction.id,
     },
-    UpdateExpression: 'set #status = :status',
+    UpdateExpression: "set #status = :status",
     ExpressionAttributeValues: {
-      ':status': 'CLOSED',
+      ":status": "CLOSED",
     },
     ExpressionAttributeNames: {
-      '#status': 'status',
+      "#status": "status",
     },
   };
 
-  const result = await dynamoDb.update(params).promise();
+  await dynamoDb.update(params).promise();
+  const { title, seller, highestBid } = auction;
+  const { amount, bidder } = highestBid;
 
-  return result;
+  const notifySeller = sqs
+    .sendMessage({
+      QueueUrl: MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: "Your Item has been sold",
+        recipient: seller,
+        body: `Congratulations your item ${title} has been sold for $${amount}.`,
+      }),
+    })
+    .promise();
+
+  const notifyBidder = sqs
+    .sendMessage({
+      QueueUrl: MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: "You won an Auction",
+        recipient: bidder,
+        body: `What a good deal! you are the winner of the auction ${title} at a price of $${amount}.`,
+      }),
+    })
+    .promise();
+
+  return Promise.all([notifyBidder, notifySeller]);
 };
