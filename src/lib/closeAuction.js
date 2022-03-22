a7038b4c6c5d787ad069ef728141fbd6750ef99b
lurdharry <azeezhammed57@gmail.com>
2022-03-22 23:31:27 +0100

    chore: send no bid message

--- src/lib/closeAuction.js
+++ src/lib/closeAuction.js
@@ -2,6 +2,7 @@ import AWS from "aws-sdk";
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const sqs = new AWS.SQS();
+
 const { AUCTIONS_TABLE_NAME, MAIL_QUEUE_URL } = process.env;
 
 export const closeAuction = async (auction) => {
@@ -22,6 +23,20 @@ export const closeAuction = async (auction) => {
   await dynamoDb.update(params).promise();
   const { title, seller, highestBid } = auction;
   const { amount, bidder } = highestBid;
+
+  if (amount === 0) {
+    await sqs
+      .sendMessage({
+        QueueUrl: MAIL_QUEUE_URL,
+        MessageBody: JSON.stringify({
+          subject: "No bid on your auction item",
+          recipient: seller,
+          body: `oh no, your item ${title} did not get any bid, better luck next time`,
+        }),
+      })
+      .promise();
+    return;
+  }
 
   const notifySeller = sqs
     .sendMessage({
